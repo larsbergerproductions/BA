71cd80096e63cefb7a746586af91bd4cce057773
Lars Berger <lars.berger@unibw.de>
2019-09-02 13:23:34 +0200

    Added chapters: Greedy Algorithmus & Anhang; added packages todonotes, dsfont etc., some other changes in formatting as well

--- Python_Project/Greedy-Algo/greedy.py
+++ Python_Project/Greedy-Algo/greedy.py
@@ -14,21 +14,40 @@ def egyptian_partition(frac):
 				candidate = Fraction(min(result)).denominator + 1
 			while Fraction(1, candidate) > frac - sum(result):
 				candidate += 1
-				if candidate % 1e6 == 0:                                # for REAL long calculations
+				if candidate % 1e6 == 0:  # for REALLY long calculations
 					print("currently tried:" + str(candidate))
 			# found right candidate
 			result.append(Fraction(1, candidate))
-			print("appended" + str(Fraction(1, candidate)))
-		# insert calculating code here!
-		
+			# print("appended" + str(Fraction(1, candidate)))
 		return result
 
 
+def find_largest_fitting_unit_fraction(frac):
+	result = Fraction(1, frac.denominator)
+	while result < frac:
+		result += Fraction(1,result.denominator)
+	return result
+
+
+def optimize_egyptian_partition(part_list):
+	
+	return None
+
+
+# frac_to_find_for = Fraction(5,129)
+# found_for_frac = find_largest_fitting_unit_fraction(frac_to_find_for)
+# print("the largest unit fraction fitting into {0} is {1}".format(frac_to_find_for, found_for_frac))
+
+# insert fraction here:
 my_frac = Fraction(41, 42)
 my_result = egyptian_partition(my_frac)
+print("\n\n\n\n")
 print(str(my_frac) + " = ", end='')
 print(*my_result, sep=' + ')
-if sum(my_result) != my_frac:
-	print("failed")
-else:
-	print("success!")
+# if sum(my_result) != my_frac:
+# 	print("failed")
+# else:
+# 	print("success!")
+#
+# optimized = optimize_egyptian_partition(my_result)
+# print(optimized)
