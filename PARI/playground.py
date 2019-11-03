aa2bc4fbd14633eee46391f47a68f5f7f093595b
Lars Berger <lars.berger@unibw.de>
2019-11-03 09:58:12 +0100

    updated playground to binary maxDenom exploration

--- PARI/playground.py
+++ PARI/playground.py
@@ -2,7 +2,6 @@ import numpy as np
 import json
 
 
-
 def load_data_from_json():
 	with open("data.json", 'r') as file:
 		json_file = json.load(file)
@@ -19,12 +18,8 @@ def is_power_of_two(n):
 
 
 data = load_data_from_json()
-reslist = []
-print(data["n"][0])
-currmax = data["greedy"]["maxTerms"][0]
-for i in range(0, len(data["greedy"]["maxDenom"])-1):
-	if data["greedy"]["maxDenom"][i] < currmax:
-		currmax = data["greedy"]["maxDenom"][i]
-		# print("reset with " + str(currmax))
-		print(data["n"][i])
-print(currmax)
+for i in range(8192, len(data["greedy"]["maxDenom"])-1):
+	if data["binary"]["maxDenom"][i] < 1e6:
+		print(str(data["n"][i]) + " : " + str(data["binary"]["maxDenom"][i]))
+		if is_power_of_two(data["binary"]["maxDenom"][i]):
+			print("is power of 2")
